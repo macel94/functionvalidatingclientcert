01c0ebd5c20007ee3171cb96ef982a0c5d50d512
francesco.belacca <francesco.belacca@agic.cloud>
2023-11-06 16:39:41 +0100

    added function and script to test it

---
+++ Scripts/ScriptsToTestFunctionGeneratingAndSendingCertificate.ps1
@@ -0,0 +1,32 @@
+﻿# Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process
+# Generate the certificate
+$cert = New-SelfSignedCertificate -DnsName "localhost" -CertStoreLocation "cert:\LocalMachine\My"
+
+# Define the export path
+$exportPath = ".\cert.cer"
+
+# Export the certificate to the specified folder
+Export-Certificate -Cert "cert:\LocalMachine\My\$($cert.Thumbprint)" -FilePath $exportPath
+
+# Read and convert the certificate to Base64
+$certBytes = Get-Content -Path $exportPath -Encoding Byte
+$base64Cert = [System.Convert]::ToBase64String($certBytes)
+
+# Prepare the header with the Base64 encoded certificate
+$headers = @{
+    "X-ARR-ClientCert" = $base64Cert
+}
+
+# Invoke the API request with the certificate in the header
+$response = Invoke-RestMethod -Uri "http://localhost:7164/api/ValidateCertAuth" -Method Get -Headers $headers
+
+$response
+
+# Output the status code
+$response.StatusCode
+
+# Output the headers
+$response.Headers
+
+# Output the response content
+$response.Content
---
+++ src/FunctionThatValidatesCertificatesInHeader/ValidateCertAuth.cs
@@ -0,0 +1,65 @@
+﻿using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace FunctionThatValidatesCertificatesInHeader
+{
+    public class ValidateCertAuth
+    {
+        private readonly ILogger _logger;
+        public ValidateCertAuth(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<DefaultFunction>();
+        }
+
+        [Function(nameof(ValidateCertAuth))]
+        public HttpResponseData RandomStringCertAuth(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger to validate the certificate.");
+            var response = req.CreateResponse(HttpStatusCode.OK);
+
+            if (req.Headers.TryGetValues("X-ARR-ClientCert", out var certs))
+            {
+                byte[] clientCertBytes = Convert.FromBase64String(certs.First());
+                X509Certificate2 clientCert = new X509Certificate2(clientCertBytes);
+
+                // Validate Thumbprint  
+                //if (clientCert.Thumbprint != "yourthumprint")
+                //{
+                //    response.StatusCode = HttpStatusCode.BadRequest;
+                //    response.WriteString("A valid client certificate was not used");
+                //    return response;
+                //}
+
+                // Validate NotBefore and NotAfter  
+                //if (DateTime.Compare(DateTime.UtcNow, clientCert.NotBefore) < 0
+                //            || DateTime.Compare(DateTime.UtcNow, clientCert.NotAfter) > 0)
+                //{
+                //    return new BadRequestObjectResult("client certificate not in alllowed time interval");
+                //}
+
+                //// Add further validation of certificate as required.  
+
+                //return new OkObjectResult(GetEncodedRandomString());
+
+                // Send the decoded certificate multiple fields to the client
+                response.WriteString("Subject: " + clientCert.Subject + "\n");
+                response.WriteString("Issuer: " + clientCert.Issuer + "\n");
+                response.WriteString("Thumbprint: " + clientCert.Thumbprint + "\n");
+                response.WriteString("NotBefore: " + clientCert.NotBefore + "\n");
+                response.WriteString("NotAfter: " + clientCert.NotAfter + "\n");
+                response.WriteString("SerialNumber: " + clientCert.SerialNumber + "\n");
+                response.WriteString("PublicKey: " + clientCert.PublicKey + "\n");
+                return response;
+            }
+
+            response.StatusCode = HttpStatusCode.BadRequest;
+            response.WriteString("A valid client certificate is not found");
+
+            return response;
+        }
+    }
+}
