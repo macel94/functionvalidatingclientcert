6eca39e63c13a161c5d49c14ec4bed519ed2d2df
francesco.belacca <francesco.belacca@agic.cloud>
2023-11-12 00:06:51 +0100

    everything updated to work just with Microsoft.Graph.Applications pwsh module

--- Scripts/IdempotentAppAndRolesCreationWithTokenValidation.ps1
+++ Scripts/IdempotentAppAndRolesCreationWithTokenValidation.ps1
@@ -1,45 +1,22 @@
+
 Import-Module Microsoft.Graph.Applications
 
 $tenantId = "6998af00-286c-4e5e-8b3e-713471e8487f"
-$subscriptionId = "6a37c895-4239-4b1e-bc34-a48c4994cc8a"
 $functionAppName = "FBelaccaFunctionTest1"
 $apiAppName = "$functionAppName-api"
 $apiAppIdentifierUri = "api://$functionAppName"
 $clientAppName = "$functionAppName-client"
 
-Write-Host "Subscription Id: $subscriptionId"
-Write-Host "Function App Name: $functionAppName"
-Write-Host "Tenant Id: $tenantId"
-Write-Host "Api App Name: $apiAppName"
-Write-Host "Api App Identifier Uri: $apiAppIdentifierUri"
-Write-Host "Client App Name: $clientAppName"
-
-#az login --tenant $tenantId
 Connect-MgGraph -NoWelcome -TenantId $tenantId -Scopes 'Application.ReadWrite.All','AppRoleAssignment.ReadWrite.All','Directory.AccessAsUser.All','Directory.ReadWrite.All'
-az account set --subscription $subscriptionId
 
-# Function to ensure service principal creation
-function Ensure-ServicePrincipal {
-    param (
-        [string]$appId
-    )
-
-    $sp = Get-MgServicePrincipal -Filter "appId eq '$appId'"
-    if (-not $sp) {
-        az ad sp create --id $appId
-        Start-Sleep -Seconds 10 # Wait for propagation
-        $sp = Get-MgServicePrincipal -Filter "appId eq '$appId'"
-    }
-    return $sp.Id.ToString()
-}
-
-$existingApiApp = az ad app list --filter "displayName eq '$apiAppName'" --query "[].appId" -o tsv
-if ([string]::IsNullOrEmpty($existingApiApp)) {
-    $apiAppId = az ad app create --identifier-uris $apiAppIdentifierUri --display-name $apiAppName --sign-in-audience "AzureADMyOrg" --query "appId" -o tsv
-    Write-Host "New Api App Id: $apiAppId"
+$existingApiApp = Get-MgApplication -Filter "displayName eq '$apiAppName'"
+if ($null -eq $existingApiApp) {
+    Write-Host "Creating api app $apiAppName"
+    $apiApp = New-MgApplication -DisplayName $apiAppName -IdentifierUris $apiAppIdentifierUri -SignInAudience "AzureADMyOrg"
+    $apiAppId = $apiApp.AppId
 } else {
-    $apiAppId = $existingApiApp
-    Write-Host "Existing Api App Id: $apiAppId"
+    Write-Host "Api app $apiAppName already exists with id $existingApiApp.AppId"
+    $apiAppId = $existingApiApp.AppId
 }
 
 $apiEntity = Get-MgApplication -Filter "AppId eq '$apiAppId'"
@@ -54,61 +31,82 @@ $appRole = @{
 }
 
 $appRoles = $apiEntity.AppRoles
-
 $existingRole = $appRoles | Where-Object { $_.DisplayName -eq $appRole.DisplayName }
 
-if ($existingRole) {
-    # If there are multiple roles with the same display name, assign the first one
-    $roleIdToAssign = $existingRole.Id
-} else {
+if ($null -eq $existingRole) {
+    Write-Host "Creating role $($appRole.DisplayName)"
     $appRoles += $appRole
+    Update-MgApplication -ApplicationId $apiAppId -AppRoles $appRoles
     $roleIdToAssign = $appRole.Id
-    Update-MgApplicationByAppId -AppId $apiAppId -AppRoles $appRoles
-}
-
-$existingClientApp = az ad app list --filter "displayName eq '$clientAppName'" --query "[].appId" -o tsv
-if ([string]::IsNullOrEmpty($existingClientApp)) {
-    $clientAppId = az ad app create --display-name $clientAppName --sign-in-audience "AzureADMyOrg" --query "appId" -o tsv
-    Write-Host "New Client App Id: $clientAppId"
 } else {
-    $clientAppId = $existingClientApp
-    Write-Host "Existing Client App Id: $clientAppId"
+    Write-Host "Role $($appRole.DisplayName) already exists with id $($existingRole.Id)"
+    $roleIdToAssign = $existingRole.Id
 }
 
-# Ensure Service Principal for API App
-$apiObjectId = Ensure-ServicePrincipal -appId $apiAppId
-# Ensure Service Principal for Client App
-$clientAppObjectId = Ensure-ServicePrincipal -appId $clientAppId
+$existingClientApp = Get-MgApplication -Filter "displayName eq '$clientAppName'"
+if ($null -eq $existingClientApp) {
+    Write-Host "Creating client app $clientAppName"
+    $clientApp = New-MgApplication -DisplayName $clientAppName -SignInAudience "AzureADMyOrg"
+    $clientAppId = $clientApp.AppId
+} else {
+    Write-Host "Client app $clientAppName already exists with id $($existingClientApp.AppId)"
+    $clientAppId = $existingClientApp.AppId
+}
 
-Write-Host "Client App Object ID: $clientAppObjectId"
-Write-Host "API App Object ID: $apiObjectId"
+# Service Principal Creation
+function Confirm-ServicePrincipal {
+    param ([string]$appId)
+    $sp = Get-MgServicePrincipal -Filter "appId eq '$appId'"
+    if ($null -eq $sp) {
+        Write-Host "Service Principal not found for appId $appId"
+        $sp = New-MgServicePrincipal -AppId $appId
+        Start-Sleep -Seconds 10
+    }
+    else{
+        Write-Host "Service Principal found for appId $appId"
+    }
+    return $sp.Id
+}
 
-$existingAssignments = Get-MgServicePrincipalAppRoleAssignment -ServicePrincipalId $clientAppObjectId
+$apiServicePrincipalId = Confirm-ServicePrincipal -appId $apiAppId
+$clientAppServicePrincipalId = Confirm-ServicePrincipal -appId $clientAppId
 
-Write-Host "Existing role assignments: $($existingAssignments | ConvertTo-Json -Depth 2)"
-
+$existingAssignments = Get-MgServicePrincipalAppRoleAssignment -ServicePrincipalId $clientAppServicePrincipalId
 if ($existingAssignments.AppRoleId -notcontains $roleIdToAssign) {
-    Write-Host "Role ID to Assign: $roleIdToAssign"
-    New-MgServicePrincipalAppRoleAssignment -ServicePrincipalId $apiObjectId -PrincipalId $clientAppObjectId -ResourceId $apiObjectId -AppRoleId $roleIdToAssign
-} else {
-    Write-Host "Role ID $roleIdToAssign is already assigned to the service principal."
+    Write-Host "Assigning role $roleIdToAssign to $clientAppServicePrincipalId"
+    New-MgServicePrincipalAppRoleAssignment -ServicePrincipalId $apiServicePrincipalId -PrincipalId $clientAppServicePrincipalId -ResourceId $apiServicePrincipalId -AppRoleId $roleIdToAssign
+}
+else {
+    Write-Host "Role $roleIdToAssign already assigned to $clientAppServicePrincipalId"
 }
 
-# Generate a new secret for the existing client app
-$clientSecret = az ad app credential reset --id $clientAppId --append --years 2 --query "password" -o tsv
+# Add Client Secret to the Client App
+$passwordCred = @{
+    "displayName" = "ClientSecretFor$functionAppName"
+    "endDateTime" = (Get-Date).AddYears(2)
+}
+# Here we need to use the clientObjectId of the clientApp so we make a get
+# Retrieve the client application entity
+$clientAppEntity = Get-MgApplication -Filter "AppId eq '$clientAppId'"
 
-if (-not [string]::IsNullOrEmpty($clientSecret)) {
-    Write-Host "Client Secret: $clientSecret"
-    Start-Sleep -Seconds 30 # Wait for propagation
-} else {
-    Write-Host "Client Secret was not generated."
+Write-Host "Creating secret for client app named $clientAppName with objectId $($clientAppEntity.Id)"
+
+# Use the Id of the client application when adding the password
+$clientSecret = Add-MgApplicationPassword -ApplicationId $($clientAppEntity.Id) -PasswordCredential $passwordCred
+
+if ($null -ne $clientSecret.SecretText) {
+    Write-Host "Secret created: $($clientSecret.SecretText), waiting 10 seconds for replication"
+    Start-Sleep -Seconds 10
+}
+else {
+    Write-Host "Secret not created"
 }
 
 # Finally Get a token using clientApp service principal and correct scope
 $body = @{
     client_id     = $clientAppId
     scope         = "$apiAppId/.default"
-    client_secret = $clientSecret
+    client_secret = $clientSecret.SecretText
     grant_type    = "client_credentials"
 }
 
