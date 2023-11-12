aebe5b0dd8a0ff516d7271c73c964f5fde2827fe
francesco.belacca <francesco.belacca@agic.cloud>
2023-11-12 15:36:27 +0100

    inserted try catch for unknown 500 error

--- src/FunctionThatValidatesCertificatesInHeader/ValidateCertAuth.cs
+++ src/FunctionThatValidatesCertificatesInHeader/ValidateCertAuth.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using System.Security.Cryptography.X509Certificates;
+using Azure;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
@@ -18,58 +19,66 @@ namespace FunctionThatValidatesCertificatesInHeader
         public HttpResponseData RandomStringCertAuth(
         [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
         {
-            _logger.LogInformation("C# HTTP trigger to validate the certificate.");
-            // Log all headers
-            foreach (var header in req.Headers)
+            try
             {
-                _logger.LogInformation($"Header '{header.Key}': '{string.Join(",", header.Value)}'");
-            }
-
-            var response = req.CreateResponse(HttpStatusCode.OK);
-
-            if (req.Headers.TryGetValues("x-forwarded-client-cert", out var certs))
-            {
-                foreach (var cert in certs)
+                _logger.LogInformation("C# HTTP trigger to validate the certificate.");
+                // Log all headers
+                foreach (var header in req.Headers)
                 {
-                    byte[] clientCertBytes = Convert.FromBase64String(cert);
-                    X509Certificate2 clientCert = new X509Certificate2(clientCertBytes);
-
-                    // Validate Thumbprint  
-                    //if (clientCert.Thumbprint != "yourthumprint")
-                    //{
-                    //    response.StatusCode = HttpStatusCode.BadRequest;
-                    //    response.WriteString("A valid client certificate was not used");
-                    //    return response;
-                    //}
-
-                    // Validate NotBefore and NotAfter  
-                    //if (DateTime.Compare(DateTime.UtcNow, clientCert.NotBefore) < 0
-                    //            || DateTime.Compare(DateTime.UtcNow, clientCert.NotAfter) > 0)
-                    //{
-                    //    return new BadRequestObjectResult("client certificate not in alllowed time interval");
-                    //}
-
-                    //// Add further validation of certificate as required.  
-
-                    //return new OkObjectResult(GetEncodedRandomString());
-
-                    // Send the decoded certificate multiple fields to the client, for example
-                    response.WriteString("Subject: " + clientCert.Subject + "\n");
-                    response.WriteString("Issuer: " + clientCert.Issuer + "\n");
-                    response.WriteString("Thumbprint: " + clientCert.Thumbprint + "\n");
-                    response.WriteString("NotBefore: " + clientCert.NotBefore + "\n");
-                    response.WriteString("NotAfter: " + clientCert.NotAfter + "\n");
-                    response.WriteString("SerialNumber: " + clientCert.SerialNumber + "\n");
-                    response.WriteString("PublicKey: " + clientCert.PublicKey + "\n");
+                    _logger.LogInformation($"Header '{header.Key}': '{string.Join(",", header.Value)}'");
                 }
+
+                var response = req.CreateResponse(HttpStatusCode.OK);
+
+                if (req.Headers.TryGetValues("x-forwarded-client-cert", out var certs))
+                {
+                    foreach (var cert in certs)
+                    {
+                        byte[] clientCertBytes = Convert.FromBase64String(cert);
+                        X509Certificate2 clientCert = new X509Certificate2(clientCertBytes);
+
+                        // Validate Thumbprint  
+                        //if (clientCert.Thumbprint != "yourthumprint")
+                        //{
+                        //    response.StatusCode = HttpStatusCode.BadRequest;
+                        //    response.WriteString("A valid client certificate was not used");
+                        //    return response;
+                        //}
+
+                        // Validate NotBefore and NotAfter  
+                        //if (DateTime.Compare(DateTime.UtcNow, clientCert.NotBefore) < 0
+                        //            || DateTime.Compare(DateTime.UtcNow, clientCert.NotAfter) > 0)
+                        //{
+                        //    return new BadRequestObjectResult("client certificate not in alllowed time interval");
+                        //}
+
+                        //// Add further validation of certificate as required.  
+
+                        //return new OkObjectResult(GetEncodedRandomString());
+
+                        // Send the decoded certificate multiple fields to the client, for example
+                        response.WriteString("Subject: " + clientCert.Subject + "\n");
+                        response.WriteString("Issuer: " + clientCert.Issuer + "\n");
+                        response.WriteString("Thumbprint: " + clientCert.Thumbprint + "\n");
+                        response.WriteString("NotBefore: " + clientCert.NotBefore + "\n");
+                        response.WriteString("NotAfter: " + clientCert.NotAfter + "\n");
+                        response.WriteString("SerialNumber: " + clientCert.SerialNumber + "\n");
+                        response.WriteString("PublicKey: " + clientCert.PublicKey + "\n");
+                    }
+
+                    return response;
+                }
+
+                response.StatusCode = HttpStatusCode.BadRequest;
+                response.WriteString("A valid client certificate is not found");
 
                 return response;
             }
-
-            response.StatusCode = HttpStatusCode.BadRequest;
-            response.WriteString("A valid client certificate is not found");
-
-            return response;
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error validating certificate");
+                return req.CreateResponse(HttpStatusCode.InternalServerError);
+            }
         }
     }
 }
